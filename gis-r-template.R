4afa9ba113387b7fa588907eaf504ad3e419865f
Angela Li <angela-li@users.noreply.github.com>
2018-10-23 11:41:18 -0500

    Add template for step-by-step workshop

---
+++ gis-r-template.R
@@ -0,0 +1,15 @@
+## Template to fill in during workshop
+
+# Load packages ----
+
+# Read in data ----
+
+# Inspect and explore data ----
+
+# Calculate population density ----
+
+# Make map ---- 
+
+# Perform geographic operations ----
+
+# Make second map ----
